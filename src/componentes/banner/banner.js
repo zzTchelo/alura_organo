6895331d8300786d594027c03ba71782d9361235
joao Marcelo <jmarcelo06@outlook.com>
2023-07-20 20:40:18 -0300

    Primeiro projeto em React

---
+++ src/componentes/banner/banner.js
@@ -0,0 +1,11 @@
+import './banner.css'
+
+function Banner () {
+    return (
+        <header className='banner'>
+            <img src="/imagens/banner.png" alt="Banner_Principal"/>
+        </header>
+    )
+}
+
+export default Banner
